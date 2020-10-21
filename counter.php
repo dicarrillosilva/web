032bc056f59e3347f806f029619c4c3857711508
Diana Carrillo <diana@Dianas-MacBook-Pro.local>
2020-10-21 17:55:16 +0200

    counter

--- counter.php
+++ counter.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$counter_name = "database.txt";
+$counter_name = "counter.txt";
 
 // Check if a text file exists.
 // If not create one and initialize it to zero.
@@ -25,26 +25,6 @@ if(!isset($_SESSION['hasVisited'])){
   fclose($f);
 }
 
-//echo "You are visitor number $counterVal to this site";
+echo "You are visitor number $counterVal to this site";
 
-$counterVal = str_pad($counterVal, 5, "0", STR_PAD_LEFT);
-  $chars = preg_split('//', $counterVal);
-  $im = imagecreatefrompng("canvas.png");
-
-  $src1 = imagecreatefrompng("$chars[1].png");
-  $src2 = imagecreatefrompng("$chars[2].png");
-  $src3 = imagecreatefrompng("$chars[3].png");
-  $src4 = imagecreatefrompng("$chars[4].png");
-  $src5 = imagecreatefrompng("$chars[5].png");
-
-  imagecopymerge($im, $src1, 0, 0, 0, 0, 56, 75, 100);
-  imagecopymerge($im, $src2, 60, 0, 0, 0, 56, 75, 100);
-  imagecopymerge($im, $src3, 120, 0, 0, 0, 56, 75, 100);
-  imagecopymerge($im, $src4, 180, 0, 0, 0, 56, 75, 100);
-  imagecopymerge($im, $src5, 240, 0, 0, 0, 56, 75, 100);
-
-  // Output and free from memory
-  header('Content-Type: image/png');
-  echo imagepng($im);
-  imagedestroy($im);
 ?>
