9b61b70075e514fe8bd57ad4c324d42d3dcfb9d1
Diana Carrillo <diana@Dianas-MacBook-Pro.local>
2020-10-22 17:15:55 +0200

    counter sql

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Diana Carrillo Silva</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<h2> DIANA CARRILLO SILVA </h2>
+				<p> <a href="portfolio.html">Portfolio</a> | <a href="blog.html">Blog</a> </p>
+			</header>
