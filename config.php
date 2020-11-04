58d2fce6ce545f035d072d1c072eacf4760a33e7
Diana Carrillo <diana@Dianas-MacBook-Pro.local>
2020-11-04 17:15:06 +0100

    counter 16

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+//Enter your desired background colour below
+define('BACKGROUND_COLOUR_HEX', '#E6EAE9');
+
+//Enter your desired text colour below
+define('TEXT_COLOUR_HEX', '#3399FF');
+
+//Enter your desired shadow colour below
+define('SHADOW_COLOUR_HEX', '#000000');
+
+//Enter a font size
+define('FONT', 20);
+
+//Enter your desired font below, this should be a ttf file and exist in the same folder as this
+define('FONT_TYPE', 'BLUEHIGD.ttf');
+
+//Enter the desired text below to append the count value (if you wish to have text there, leave blank otherwise). Please note the space at the start of the text. 
+define('IMAGE_TEXT', '');
+
